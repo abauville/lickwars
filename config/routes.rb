8e542f6c43e61bb98e3822fe27983e1a1b862471
Arthur Bauville <a.bauville@gmail.com>
2022-03-01 16:37:04 +0900

    empty exercise controller with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :exercises, only: %i[index new create edit update delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
