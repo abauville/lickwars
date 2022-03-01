82cb0a0b89145359fce9b61ab13d6687fcec2db3
Markie Choi <markie054@gmail.com>
2022-03-01 16:39:57 +0900

    fixed bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_052010) do
+ActiveRecord::Schema.define(version: 2022_03_01_063158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,21 @@ ActiveRecord::Schema.define(version: 2022_03_01_052010) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "exercise_id", null: false
+    t.boolean "is_question", default: false, null: false
+    t.integer "status", default: 0, null: false
     t.index ["exercise_id"], name: "index_musics_on_exercise_id"
     t.index ["user_id"], name: "index_musics_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "exercise_id", null: false
+    t.bigint "user_id", null: false
+    t.text "content"
+    t.integer "vote", default: 1, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["exercise_id"], name: "index_reviews_on_exercise_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,4 +73,6 @@ ActiveRecord::Schema.define(version: 2022_03_01_052010) do
   add_foreign_key "exercises", "users"
   add_foreign_key "musics", "exercises"
   add_foreign_key "musics", "users"
+  add_foreign_key "reviews", "exercises"
+  add_foreign_key "reviews", "users"
 end
