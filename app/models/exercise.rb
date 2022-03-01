4ce5e3299eec4d7a6634a47c52c3def9b00f7407
nairdesigns <nairbdg@gmail.com>
2022-03-01 21:21:24 +0900

    done

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -5,8 +5,21 @@ class Exercise < ApplicationRecord
   validates :difficulty, numericality: { greater_than_or_equal_to: 0 }
   validates :name, presence: true, length: { minimum: 3 }
 
-
   def reference_music
     musics.where(user: user)
+  end
+
+  def diff_level
+    @exercises = Exercise.all
+    @exercises.each do |exercise|
+      if exercise.difficulty.between?(1, 4)
+        @answer = "easy"
+      elsif exercise.difficulty.between?(4, 7)
+        @answer = "medium"
+      else
+        @answer = "hard"
+      end
+    end
+    @answer
   end
 end
