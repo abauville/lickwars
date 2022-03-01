bdec0d1fa6524f56b531e9db7af1fb1b477332d2
Arthur Bauville <a.bauville@gmail.com>
2022-03-01 16:55:35 +0900

    passed exercises to the index page and put raise in other actions

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,16 +1,21 @@
 class ExercisesController < ApplicationController
   def index
+    @exercises = Exercise.all
   end
 
   def new
+    raise
   end
 
   def create
+    raise
   end
 
   def edit
+    raise
   end
 
   def update
+    raise
   end
 end
