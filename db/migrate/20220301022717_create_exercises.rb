79fcc32593a4e0f40fd58d371669dd126a979396
Arthur Bauville <a.bauville@gmail.com>
2022-03-01 18:32:48 +0900

    dropped and recreate the db because I had a hard time dropping the reference to music in exercise

--- db/migrate/20220301022717_create_exercises.rb
+++ db/migrate/20220301022717_create_exercises.rb
@@ -2,7 +2,6 @@ class CreateExercises < ActiveRecord::Migration[6.1]
   def change
     create_table :exercises do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :music, null: false, foreign_key: true
       t.float :difficulty
       t.string :name, null: false
       t.text :description, null: false, default: ""
