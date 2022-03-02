629d482eaedd420576bf0891da789a7ae246d9e9
nairdesigns <nairbdg@gmail.com>
2022-03-02 17:39:10 +0900

    pop over attempt

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "controllers"
-import "bootstrap"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "controllers";
+import "bootstrap";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
+
+$(document).ready(function () {
+  $('[data-toggle="popover"]').popover({
+    placement: "top",
+    trigger: "hover",
+  });
+});
