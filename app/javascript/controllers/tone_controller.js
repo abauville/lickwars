a8ae10a01ddacb17e80df8501578defffe7391b2
nairdesigns <nairbdg@gmail.com>
2022-03-03 15:18:40 +0900

    was able to show instance of music on the show page

--- app/javascript/controllers/tone_controller.js
+++ app/javascript/controllers/tone_controller.js
@@ -12,13 +12,11 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   log(params) {
-    console.log("test?");
-    //create a synth and connect it to the main output (your speakers)
-
     const synth = new Tone.Synth().toDestination();
-
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n");
+    const now = Tone.now();
+    synth.triggerAttackRelease("C4", "8n", now);
+    synth.triggerAttackRelease("E4", "8n", now + 0.5);
+    synth.triggerAttackRelease("G4", "8n", now + 1);
   }
 
   // static targets = ["output"];
