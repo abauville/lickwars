7829665d508d141905453490a095a7756cf86923
nairdesigns <nairbdg@gmail.com>
2022-03-03 16:06:38 +0900

    working functionally can be optimized for more than 4 notes

--- app/javascript/controllers/tone_controller.js
+++ app/javascript/controllers/tone_controller.js
@@ -28,9 +28,10 @@ export default class extends Controller {
     console.log(splitNotesArray);
     const synth = new Tone.Synth().toDestination();
     const now = Tone.now();
-    synth.triggerAttackRelease("C4", "8n", now);
-    synth.triggerAttackRelease("E4", "8n", now + 0.5);
-    synth.triggerAttackRelease("G4", "8n", now + 1);
+    synth.triggerAttackRelease(`${splitNotesArray[0]}4`, "8n", now);
+    synth.triggerAttackRelease(`${splitNotesArray[1]}4`, "8n", now + 0.5);
+    synth.triggerAttackRelease(`${splitNotesArray[2]}4`, "8n", now + 1);
+    synth.triggerAttackRelease(`${splitNotesArray[3]}4`, "8n", now + 1.5);
   }
 
   // static targets = ["output"];
