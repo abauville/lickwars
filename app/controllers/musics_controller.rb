c6a8f9e2f560eea57178fa3fd602c9c8dfad6f84
Kanae Maki <m14ky.gu@gmail.com>
2022-03-04 11:02:00 +0900

    pundit generate

---
+++ app/controllers/musics_controller.rb
@@ -0,0 +1,30 @@
+class MusicsController < ApplicationController
+  def new
+    @music = current_user.musics.new
+  end
+
+  def create
+    exercise = Exercise.find(params[:exercise_id])
+    @music = current_user.musics.new(music_params)
+    @music.exercise = exercise
+    if @music.save
+      redirect_to excercises_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def music_params
+    params.require(:music).permit(
+      :bpm,
+      :key_signature,
+      :mode,
+      :notes,
+      :chords,
+      :note_values,
+      :chord_values
+    )
+  end
+end
