b2215c669b5a6ff1621cb5c08b1c5119197d803a
Arthur Bauville <a.bauville@gmail.com>
2022-03-05 20:08:35 +0900

    quick fix to issue

--- app/javascript/components/changeScrollButtonStyle.js
+++ app/javascript/components/changeScrollButtonStyle.js
@@ -1,18 +1,19 @@
 
 const teacherButton = document.getElementById('teacher-ex-card-button-add')
 
-const buttonContainer = document.getElementById("button-container")
+  const buttonContainer = document.getElementById("button-container")
+  const footer = document.getElementById('footer')
 
-const footer = document.getElementById('footer')
+  const checkOffset = () => {
+    if (teacherButton) {
+      if (teacherButton.offsetTop + Math.abs(buttonContainer.getBoundingClientRect().y) >= footer.offsetTop - 175) {
+        teacherButton.style.position = "absolute"
+      }
 
-const checkOffset = () => {
-  if (teacherButton.offsetTop + Math.abs(buttonContainer.getBoundingClientRect().y) >= footer.offsetTop - 175) {
-    teacherButton.style.position = "absolute"
+      if ((window.scrollY + window.innerHeight) <= footer.offsetTop) {
+        teacherButton.style.position = "fixed"
+      }
+    }
   }
 
-  if ((window.scrollY + window.innerHeight) <= footer.offsetTop) {
-    teacherButton.style.position = "fixed"
-  }
-}
-
-export default document.addEventListener(('scroll'), checkOffset);
+  export default document.addEventListener(('scroll'), checkOffset);
