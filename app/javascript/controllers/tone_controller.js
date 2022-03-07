80edc292a6182436951bbf9037aa3b280cc9f764
nairdesigns <nairbdg@gmail.com>
2022-03-07 11:58:05 +0900

    can't get it to work

--- app/javascript/controllers/tone_controller.js
+++ app/javascript/controllers/tone_controller.js
@@ -1,26 +1,27 @@
 import { Controller } from "stimulus";
 import { Music } from "../models/music";
 import { BoomBox } from "../models/boom_box";
+import { Piano } from "@tonejs/piano";
 
 export default class extends Controller {
   static values = {
     question: String,
     attempt: String,
-    bpm: Number
+    bpm: Number,
   };
 
   connect() {
     console.log("bpm", this.bpmValue);
     this.boomBox = new BoomBox();
-    this.question = new Music(this.questionValue, this.bpmValue)
+    this.question = new Music(this.questionValue, this.bpmValue);
   }
 
   play_question(event) {
-    this.boomBox.play(this.question)
+    this.boomBox.play(this.question);
   }
 
   play_attempt(event) {
-    const attempt = new Music(this.attemptValue, this.bpmValue)
-    this.boomBox.play(attempt)
+    const attempt = new Music(this.attemptValue, this.bpmValue);
+    this.boomBox.play(attempt);
   }
 }
--- app/javascript/models/boom_box.js
+++ app/javascript/models/boom_box.js
@@ -1,24 +1,23 @@
 import * as Tone from "tone";
+import { Piano } from "@tonejs/piano";
 
 export class BoomBox {
   constructor() {
-    this.synth = new Tone.Synth().toDestination();
+    this.piano = new Piano({ velocities: 5 }).toDestination();
   }
 
   play(music) {
     Tone.start();
-    const playbackArrays = music.playbackArrays()
-    const noteSequence = playbackArrays[0]
-    const noteLengths = playbackArrays[1]
-
-    let counter = 0;
-    const seq = new Tone.Part(
-      (time, note) => {
-          this.synth.triggerAttackRelease(note, noteLengths[counter], time);
-          counter++;
-        },
-        noteSequence,
-    ).start();
-    Tone.Transport.start();
+    this.piano.load().then(() => {
+      const playbackArrays = music.playbackArrays();
+      const noteSequence = playbackArrays[0];
+      const noteLengths = playbackArrays[1];
+      let counter = 0;
+      const seq = new Tone.Part((time, note) => {
+        this.piano.keyUp(note, time);
+        counter++;
+      }, noteSequence).start();
+      Tone.Transport.start();
+    });
   }
 }
