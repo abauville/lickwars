00a31290cf3ba9f02499f241984b5300b6afbb9a
nairdesigns <nairbdg@gmail.com>
2022-03-07 13:12:49 +0900

    synth

--- app/javascript/models/boom_box.js
+++ app/javascript/models/boom_box.js
@@ -2,23 +2,31 @@ import * as Tone from "tone";
 
 export class BoomBox {
   constructor() {
-    this.synth = new Tone.Synth().toDestination();
+    this.synth = new Tone.Synth({
+      noise: {
+        type: "pink",
+        playbackRate: 0.1,
+      },
+      envelope: {
+        attack: 0.5,
+        decay: 2,
+        sustain: 0.5,
+        release: 3,
+      },
+    }).toDestination();
   }
 
   play(music) {
     Tone.start();
-    const playbackArrays = music.playbackArrays()
-    const noteSequence = playbackArrays[0]
-    const noteLengths = playbackArrays[1]
+    const playbackArrays = music.playbackArrays();
+    const noteSequence = playbackArrays[0];
+    const noteLengths = playbackArrays[1];
 
     let counter = 0;
-    const seq = new Tone.Part(
-      (time, note) => {
-          this.synth.triggerAttackRelease(note, noteLengths[counter], time);
-          counter++;
-        },
-        noteSequence,
-    ).start();
+    const seq = new Tone.Part((time, note) => {
+      this.synth.triggerAttackRelease(note, noteLengths[counter], time);
+      counter++;
+    }, noteSequence).start();
     Tone.Transport.start();
   }
 }
