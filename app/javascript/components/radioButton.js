73f510b4c324af254df341a44c70b07fc4cc48ca
nairdesigns <nairbdg@gmail.com>
2022-03-08 12:51:16 +0900

    button is showing

--- app/javascript/components/radioButton.js
+++ app/javascript/components/radioButton.js
@@ -1,6 +1,8 @@
 $('[name="event"]').on("change", function () {
   if ($(this).attr("id") == "walkEvent") {
-    $("#place").text("Meeting point: ");
+    $("#place").html(
+      "<p class='lw-button lw-btn-sm text-center mx-2' data-action='click->tone#play_attempt'><i class='far fa-play-circle'></i>Attempt</p>"
+    );
   } else {
     $("#place").text("Location: ");
   }
