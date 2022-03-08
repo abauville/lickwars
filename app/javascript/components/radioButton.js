2cf8d68c7f7ea5ce3b008e0bb750d0a0807c8fa7
nairdesigns <nairbdg@gmail.com>
2022-03-08 14:01:57 +0900

    working good

--- app/javascript/components/radioButton.js
+++ app/javascript/components/radioButton.js
@@ -1,11 +1,11 @@
 $('[name="event"]').on("change", function () {
   if ($(this).attr("id") == "walkEvent") {
     $("#place").html(
-      "<p class='lw-button lw-btn-sm text-center mx-2' data-action='click->tone#play_attempt'><i class='far fa-play-circle'></i>Attempt</p>"
+      "<p class='lw-button lw-btn-sm  text-center mb-0 mx-2' data-action='click->tone#play_attempt'><i class='far fa-play-circle'></i>Attempt</p>"
     );
   } else {
     $("#place").html(
-      " <p id='place' name='meeting_or_location' class='lw-button lw-btn-sm text-center mb-0 mx-2' data-action='click->tone#play_question'>Play Melody</p>"
+      " <p id='place' name='meeting_or_location' class='lw-button lw-btn-sm  text-center mb-0 mx-2' data-action='click->tone#play_question'>Play Melody</p>"
     );
   }
 });
